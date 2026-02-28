fcd268f6e7b311f1974dbf1bab159e6d13cb7caa
Ian Wilkey <iwilkey2@illinois.edu>
2026-02-27 19:33:16 -0600

    boilderplate

---
+++ lib/sigma.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// Author: Ian Wilkey and Barney Jin
+final class SigmaApp extends StatelessWidget {
+
+  const SigmaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sigma',
+      home: const Placeholder(),
+    );
+  }
+
+}
